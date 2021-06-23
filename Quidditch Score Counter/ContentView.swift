e2e5f8165568de7a2583f08fa1738e89c1e4dd68
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-06-23 19:41:52 +0530

    Initial Commit

---
+++ Quidditch Score Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quidditch Score Counter
+//
+//  Created by Soumyattam Dey on 23/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quidditch Score Counter/Quidditch_Score_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quidditch_Score_CounterApp.swift
+//  Quidditch Score Counter
+//
+//  Created by Soumyattam Dey on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Quidditch_Score_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
